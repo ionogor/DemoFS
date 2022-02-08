0d24ca07ffe1408b193784d9d87214f8690f56fc
Ogor Ion <iongor87@gmail.com>
2022-02-08 16:32:56 +0200

    add new commit from new branch

--- demoGit/demoGit/Program.cs
+++ demoGit/demoGit/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Vania!");
+Console.WriteLine("Hello, Doina!");
+Console.WriteLine("Hello, Vania!");
