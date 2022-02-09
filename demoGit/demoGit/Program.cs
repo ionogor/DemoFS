2cfeb0d67c7d56b982647bdb9a361fcc46c5a197
Ogor Ion <iongor87@gmail.com>
2022-02-09 13:55:41 +0200

    delete Progams.cs

--- demoGit/demoGit/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Vania!");
-Console.WriteLine("Hello, Doina!");
-Console.WriteLine("Hello, Vania!");
