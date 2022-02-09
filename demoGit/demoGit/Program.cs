0306f27cb627825e93ccd7400d9f2ce9bc20ecc6
Ogor Ion <iongor87@gmail.com>
2022-02-09 15:04:12 +0200

    Add function

---
+++ demoGit/demoGit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoGit
+{
+    class Program
+    {
+        public void Show(string data)
+        {
+            global::System.Console.WriteLine(data);
+        }
+    }
+}
