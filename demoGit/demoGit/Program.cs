c76ce0067484dfd89df3e8ba11d00ee81384973e
Ogor Ion <iongor87@gmail.com>
2022-02-09 13:20:50 +0200

    #2 commit

--- demoGit/demoGit/Program.cs
+++ demoGit/demoGit/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.World("Hello test");
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Vania!");
