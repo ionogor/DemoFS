d430164b4c2b5074a04861bd47b441ec9bb7bf57
Ogor Ion <iongor87@gmail.com>
2022-02-09 13:30:54 +0200

    add new info

--- demoGit/demoGit/Program.cs
+++ demoGit/demoGit/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.World("Hello test");
-
+dasdasdasdsadasd
+dasdasdasdsadasddasdas
+dasdasdasdsadasddasdasdsa
+222
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Vania!");
